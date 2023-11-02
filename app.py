0f5b4cd3ab45c9b0a58bc7f2d57255abec06ac67
Amarasiri Gunawardhana Shehan Randika <115799365+shehanrandika99@users.noreply.github.com>
2023-11-02 23:42:12 +0530

    write  flask app
    Added flask app easy ocr library using

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import easyocr
+from PIL import Image
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return render_template('index.html', error="No file part")
+        file = request.files['file']
+        if file.filename == '':
+            return render_template('index.html', error="No selected file")
+        if file:
+            image = Image.open(file)
+            reader = easyocr.Reader(['en'])  # Set the languages you want to detect here
+            results = reader.readtext(image)
+            return render_template('index.html', results=results, filename=file.filename)
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
